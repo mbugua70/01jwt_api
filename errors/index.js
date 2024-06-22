7f4f3a6bfa0aaaf35ec4efd97e90cde64019fd21
john mbugua <johnymbugua1997@gmail.com>
2024-06-22 09:03:14 +0300

    jwt and handling errors using class function

---
+++ errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require("../errors/custom-error");
+const UnAuthorized = require("../errors/unauthorized");
+const BadRequest = require("../errors/bad_request");
+
+module.exports = {
+  CustomAPIError,
+  UnAuthorized,
+  BadRequest,
+};
